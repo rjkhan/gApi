359a146cdfe849a0328560e964e63d9ad3d9697b
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-03 09:37:48 +0200

    plot issue

--- server.R
+++ server.R
@@ -6,18 +6,49 @@
 #
 
 library(shiny)
-
+library(geocodeApi)
+library(plotly)
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  library(geocodeApi)
+  randomVals <- eventReactive(input$goButton, {
+    print(length(input$caption))
+    if(input$caption == "")
+      id <<- showNotification(paste("Enter something to Search"), duration = 3, closeButton = TRUE, type = "error")
+    else
+    {
+      if(!is.null(id)) removeNotification(id)
+      input$caption
+    }
   })
+  
+  # # Show the first "n" observations ----
+  # output$view <- renderTable({
+  #   library(geocodeApi)
+  #   library(plotly)
+  #   result <- search_place("kfc")
+  #   Animals <- result$country
+  #   rate <- result$rating
+  #   country <- result$country
+  #   data <- data.frame(Animals, rate, country)
+  #   
+  #   plot_ly(data, x = ~country, y = ~result$rating, type = 'bar', name = 'rate' ) %>%
+  #     
+  #     layout(yaxis = list(title = 'rating' , dtick = 0.5), barmode = 'stack')
+  # })
+  
+  output$plot <- renderPlot({
+    # print(randomVals())
+      library(geocodeApi)
+      library(plotly)
+      result <- search_place(randomVals())
+      Animals <- result$country
+      rate <- result$rating
+      country <- result$country
+      data <- data.frame(Animals, rate, country)
 
+      plot_ly(data, x = ~country, y = ~result$rating, type = 'bar', name = 'rate' ) %>%
+      layout(yaxis = list(title = 'rating' , dtick = 0.5), barmode = 'stack')
+    # hist(randomVals())
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,24 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
+
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
+  
+  titlePanel("Hello Shiny!"),
+  
   sidebarLayout(
+    
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      textInput(inputId = "caption",
+                label = "Search:",
+                value = "",
+                placeholder = "Enter a name of place kfc etc"),
+      actionButton("goButton", "Go!")
     ),
-
-    # Show a plot of the generated distribution
+    
     mainPanel(
-      plotOutput("distPlot")
+      h3(textOutput("caption", container = span)),
+      plotOutput("plot")
+      # tableOutput("view")
     )
   )
 ))
